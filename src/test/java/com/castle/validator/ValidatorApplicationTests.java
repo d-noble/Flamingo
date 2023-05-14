c564aefffbf1e87ec45f756b1f570621ebbd097f
toonie <alexander.noble@live.com>
2023-05-13 21:43:32 -0400

    Test failure

--- src/test/java/com/castle/validator/ValidatorApplicationTests.java
+++ src/test/java/com/castle/validator/ValidatorApplicationTests.java
@@ -1,5 +1,7 @@
 package com.castle.validator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,8 @@ class ValidatorApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void exampleFailute() {
+		assertEquals(1, 2);
+	}
 }
